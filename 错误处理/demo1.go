6f2ceb298c749bdfc71e733cd2759f63dd917b7d
lnhj999 <lnhj999@outlook.com>
2019-01-15 21:48:53 +0800

    错误处理

---
+++ 错误处理/demo1.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func echo(request string) (response string,err error) {
+	if request == "" {
+		err = errors.New("empty request")
+		return
+	}
+	response = fmt.Sprintf("echo:%s",request)
+	return
+}
+
+func main() {
+	//示例1
+	for _,req := range []string{"","hello!"}{
+		fmt.Printf("request:%s\n",req)
+		resp,err := echo(req)
+		if err != nil {
+			fmt.Printf("error:%s\n",err)
+			continue
+		}
+		fmt.Printf("reponse:%s\n",resp)
+	}
+	fmt.Println()
+	//Output:
+	//request:
+	//error:empty request
+	//request:hello!
+	//reponse:echo:hello!
+
+	//示例2
+	err1 := fmt.Errorf("invalid contents:%s","#s%")
+	err2 := errors.New(fmt.Sprintf("invalid contents:%s","#s%"))
+	if err1.Error() == err2.Error() {
+		fmt.Println("The error messages in err1 and err2 are the same")
+	}
+	//Output:
+	//The error messages in err1 and err2 are the same
+}
